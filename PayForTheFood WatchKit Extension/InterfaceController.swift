0387ea51ee011b7f05732e2ef6515c418fb56557
Timothy Horng <thorng@asu.edu>
2015-10-24 15:41:28 -0700

    Initial Commit

---
+++ PayForTheFood WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  PayForTheFood WatchKit Extension
+//
+//  Created by Timothy Horng on 10/24/15.
+//  Copyright © 2015 Timothy Horng. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
